69098993d0b15ea0abd05fbdb5d4a9a47cbcf7a2
David Hide <d.hide@live.co.uk>
2018-05-25 21:58:36 +0100

    Added test cases and test script

--- index.test.js
+++ index.test.js
@@ -1,8 +1,18 @@
-const readableSeconds = require("./index.js");
+const readableSeconds = require("./index");
 
 console.log("Starting tests");
 
 const resOne = readableSeconds(3660);
 if (resOne !== "1 hour and 1 minute") {
+  throw new Error("Expected response not received");
+}
+
+const resTwo = readableSeconds(0);
+if (resTwo !== "") {
+  throw new Error("Expected empty string");
+}
+
+const resThree = readableSeconds(60);
+if (resThree !== "1 minute") {
   throw new Error("Expected response not received");
 }
